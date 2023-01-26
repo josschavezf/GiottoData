43a9229e90a4f7565ec94a3fe061446d03d2605a
jiajic <72078254+jiajic@users.noreply.github.com>
2023-01-26 10:19:53 -0500

    Update 0.2.0
    - Add zzz.R with version print
    - add test subobjects that can be loaded
    - add utility for getting the GiottoData library location

---
+++ R/test_objects.R
@@ -0,0 +1,87 @@
+
+#' @title Load a mini giotto subobject
+#' @name loadSubObjectMini
+#' @description Utility function to load a mini giotto suite S4 subobject.
+#' Can be useful for testing functions quickly or providing contained examples.
+#' @param x subobject type to load
+#' @param idx which of multiple example subobjects to load when more than one
+#' is available (see \code{\link{listSubObjectMini}})
+#' @export
+loadSubObjectMini = function(x, idx = 1L) {
+
+  avail_obj_dt = list_subobject_mini()
+  data_path = avail_obj_dt[type == x & index == idx, path]
+
+  load_data = readRDS(file = data_path)
+
+  # wrapped objects
+  if(x %in% c('giottoPoins', 'giottoPolygon')) {
+    load_data = Giotto::unwrap(load_data)
+  }
+
+  if(x == 'giottoLargeImage') {
+    original_path = load_data@file_path
+    new_path = gsub(pattern = '.*[/]GiottoData/', replacement = '', x = original_path)
+    new_path = paste0(gDataDir(), new_path)
+    load_data = Giotto:::reconnect_giottoLargeImage(giottoLargeImage = load_data,
+                                                    image_path = new_path)
+  }
+
+  if(x == 'giottoImage') {
+    original_path = load_data@file_path
+    new_path = gsub(pattern = '.*[/]GiottoData/', replacement = '', x = original_path)
+    new_path = paste0(gDataDir(), new_path)
+    load_data = Giotto:::reconnect_giottoImage_MG(giottoImage = load_data,
+                                                  image_path = new_path)
+  }
+
+  return(load_data)
+
+}
+
+
+#' @title List available mini giotto subobjects
+#' @name listSubObjectMini
+#' @description Lists the available mini giotto subobjects to load. Items are first
+#' classified by the type of subobject, then by the index number which is used
+#' to tell apart files when there is more than one example subobject of that type
+#' available. The final column is the file name.
+#' @param x subobject type (NULL lists all subobject types)
+#' @export
+listSubObjectMini = function(x = NULL) {
+
+  avail_obj_dt = list_subobject_mini()
+  avail_obj_dt_show = copy(avail_obj_dt)[, path := NULL]
+  data.table::setcolorder(avail_obj_dt_show, neworder = c('type', 'index', 'file'))
+
+  if(is.null(x)) return(avail_obj_dt_show)
+  else return(avail_obj_dt_show[type == x,])
+
+}
+
+
+#' @title List available subobjects data
+#' @name list_subobject_mini
+#' @description Utility function to list the available saved subobjects. This is
+#' the internal function which returns a version of the table with the full pathfiles
+#' and is not prettied or subsetted by type. See \code{\link{listSubObjectMini}}
+#' for the external version which provides those functions.
+#' @keywords internal
+list_subobject_mini = function() {
+
+  miniobj_path = paste0(gDataDir(), '/Mini_objects/subobjects')
+
+  avail_obj = list.files(path = miniobj_path, full.names = TRUE, recursive = TRUE)
+  avail_obj_dt = data.table::data.table(file = basename(avail_obj), path = avail_obj)
+  avail_obj_dt[, type := gsub(pattern = paste0(miniobj_path, '/'), replacement = '', x = path)]
+  avail_obj_dt[, type := gsub(pattern = '[/].*', replacement = '', x = type)]
+
+  avail_obj_dt[, index := seq_along(path), by = type]
+
+  return(avail_obj_dt)
+
+}
+
+
+
+
---
+++ inst/Mini_objects/Mini_objects_script.R
@@ -0,0 +1,148 @@
+
+## MINI OBJECTS script for use in subobject testing ##
+
+#remotes::install_github("drieslab/GiottoData")
+library(GiottoData) # devtools::load_all()
+
+#remotes::install_github("drieslab/Giotto@suite_dev")
+library(Giotto)
+
+# 0. preparation ####
+# ----------------- #
+
+viz = loadGiottoMini('vizgen')
+
+
+
+
+
+# 1. extract subobjects ####
+# ----------------- #
+
+spat_locs = get_spatial_locations(gobject = viz,
+                                  spat_unit = 'aggregate',
+                                  spat_loc_name = 'raw',
+                                  output = 'spatLocsObj')
+
+del_net = get_spatialNetwork(gobject = viz,
+                             spat_unit = 'aggregate',
+                             output = 'spatialNetworkObj',
+                             name = 'Delaunay_network')
+
+expr_vals_raw = get_expression_values(gobject = viz,
+                                      spat_unit = 'aggregate',
+                                      values = 'raw',
+                                      output = 'exprObj')
+
+expr_vals_norm = get_expression_values(gobject = viz,
+                                       spat_unit = 'aggregate',
+                                       values = 'normalized',
+                                       output = 'exprObj')
+
+mg_enr = get_spatial_enrichment(gobject = viz,
+                                enrichm_name = 'cluster_metagene',
+                                spat_unit = 'aggregate',
+                                output = 'spatEnrObj')
+
+gpoly = get_polygon_info(gobject = viz,
+                         polygon_name = 'aggregate',
+                         return_giottoPolygon = TRUE)
+
+gpoints = viz@feat_info$rna
+
+cm = Giotto:::get_cell_metadata(gobject = viz,
+                                spat_unit = 'aggregate',
+                                feat_type = 'rna',
+                                output = 'cellMetaObj')
+
+fm = Giotto:::get_feature_metadata(gobject = viz,
+                                   spat_unit = 'aggregate',
+                                   feat_type = 'rna',
+                                   output = 'featMetaObj')
+
+dim_red_umap = get_dimReduction(gobject = viz,
+                                spat_unit = 'aggregate',
+                                feat_type = 'rna',
+                                reduction_method = 'umap',
+                                name = 'umap',
+                                output = 'dimObj')
+
+dim_red_pca = get_dimReduction(gobject = viz,
+                               spat_unit = 'aggregate',
+                               feat_type = 'rna',
+                               reduction_method = 'pca',
+                               name = 'pca',
+                               output = 'dimObj')
+
+nn = get_NearestNetwork(gobject = viz,
+                        spat_unit = 'aggregate',
+                        feat_type = 'rna',
+                        nn_network_to_use = 'sNN',
+                        network_name = 'sNN.pca',
+                        output = 'nnNetObj')
+
+# images
+DAPI_z0_image_path = paste0(data_path, '/', 'images/mini_dataset_dapi_z0.jpg')
+polyT_z0_image_path = paste0(data_path, '/', 'images/mini_dataset_polyT_z0.jpg')
+
+# x and y information from original script
+ultra_mini_extent = terra::ext(c(6400.029, 6900.037, -5150.007, -4699.967 ))
+
+image_paths = c(DAPI_z0_image_path, polyT_z0_image_path)
+image_names = c('dapi_z0', 'polyT_z0')
+
+imagelist = createGiottoLargeImageList(raster_objects = image_paths,
+                                       names = image_names,
+                                       negative_y = TRUE,
+                                       extent = ultra_mini_extent)
+
+
+
+# 2. save subobjects ####
+# ----------------- #
+
+# you need to use your local GiottoData repo
+giottodata_repo = '/Users/gsi-local/Documents/GitHub/GiottoData/'
+miniobj_path = paste0(giottodata_repo, 'inst/Mini_objects/subobjects/')
+
+# expression
+saveRDS(expr_vals_raw, file = paste0(miniobj_path, '/', 'exprObj/viz_agg_expr_raw.RDS'))
+saveRDS(expr_vals_norm, file = paste0(miniobj_path, '/', 'exprObj/viz_agg_expr_norm.RDS'))
+
+# cell metadata
+saveRDS(cm, file = paste0(miniobj_path, '/', 'cellMetaObj/viz_agg_cellmeta.RDS'))
+
+# feat metadata
+saveRDS(fm, file = paste0(miniobj_path, '/', 'featMetaObj/viz_agg_featmeta.RDS'))
+
+# spatial locations
+saveRDS(spat_locs, file = paste0(miniobj_path, '/', 'spatLocsObj/viz_agg_spatlocs.RDS'))
+
+# spatial network
+saveRDS(del_net, file = paste0(miniobj_path, '/', 'spatialNetworkObj/viz_agg_spatnet_del.RDS'))
+
+# spatial enrichment
+saveRDS(mg_enr, file = paste0(miniobj_path, '/', 'spatEnrObj/viz_agg_metagene.RDS'))
+
+# nearest neighbor network
+saveRDS(nn, file = paste0(miniobj_path, '/', 'nnNetObj/viz_agg_sNN.RDS'))
+
+# dim reduction
+saveRDS(dim_red_pca, file = paste0(miniobj_path, '/', 'dimObj/viz_agg_pca.RDS'))
+saveRDS(dim_red_umap, file = paste0(miniobj_path, '/', 'dimObj/viz_agg_umap.RDS'))
+
+# giottoPoints
+gpoints = Giotto::wrap(gpoints)
+saveRDS(gpoints, file = paste0(miniobj_path, '/', 'giottoPoints/viz_agg_gpoints.RDS'))
+
+# giottoPolygon
+gpoly = Giotto::wrap(gpoly)
+saveRDS(gpoly, file = paste0(miniobj_path, '/', 'giottoPolygon/viz_agg_gpoly.RDS'))
+
+# largeImages
+saveRDS(imagelist[[1]], file = paste0(miniobj_path, '/', 'giottoLargeImage/viz_dapi_z0.RDS'))
+saveRDS(imagelist[[2]], file = paste0(miniobj_path, '/', 'giottoLargeImage/viz_polyT_z0.RDS'))
+
+
+
+
