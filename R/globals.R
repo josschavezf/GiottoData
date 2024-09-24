40b584852aa400533761c41f8adbd143b4a0344f
josschavezf <joselynchavezf@gmail.com>
2024-09-24 17:46:20 -0400

    run biocthis style

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,5 @@
 utils::globalVariables(names = c(
     ":=", ".N", ".SD", ".",
     "get_SODB_dataset", "check_SODB_adata",
-    "list_SODB_datasets", "list_SODB_dataset_experiments")
-)
+    "list_SODB_datasets", "list_SODB_dataset_experiments"
+))
